7c1d795746a6d07847ef2c9e839d8fb778f125d3
Brett Viren <brett.viren@gmail.com>
2017-04-17 11:37:01 -0400

    Clean up manual building, include styles.

--- manuals/publish.el
+++ manuals/publish.el
@@ -5,45 +5,43 @@
 (message (format "org-mode version %s" org-version))
 
 ;; https://github.com/fniessen/org-html-themes
-;; $ emacs --batch -Q -l publish.el --eval '(org-publish-project "wct-manual")'
-
-
+;; $ emacs --batch -Q -l ../publish.el --eval '(org-publish-project "wct-manual")'
+;;; Note, this is meant to be run in a sub directory of that which
+;;; holds the Org source.
 (setq org-publish-project-alist
       '(("wct-manual"
          :components (
                       "wct-manual-html"
-                      "wct-manual-styles"
 ;; need to get some latex to deal with begin_hint, begin_warning, etc
-;;                      "wct-manual-pdf"
                       "wct-manual-md"
                       ))
 
-        ("wct-manual-html"
-         :base-directory "."
-         :publishing-directory "web"
+        ("wct-index-html"
+         :base-directory ".."
+         :publishing-directory "."
          :publishing-function (org-html-publish-to-html)
          :base-extension "does-not-exist"
-         :include ("manual.org" "index.org")
+         :include ("index.org")
+         :recursive nil
+         :htmlized-source t
+         :auto-sitemap nil
+         :makeindex nil
+         )
+        ("wct-manual-html"
+         :base-directory ".."
+         :publishing-directory "."
+         :publishing-function (org-html-publish-to-html)
+         :base-extension "does-not-exist"
+         :include ("manual.org")
          :recursive nil
          :htmlized-source t
          :auto-sitemap nil
          :makeindex nil
          )
 
-        ("wct-manual-styles"
-         :base-directory "org-html-themes/styles"
-         :publishing-directory "web/styles/"
-         :publishing-function (org-publish-attachment)
-         :base-extension "css\\|js\\|png"
-         :recursive t
-         :htmlized-source t
-         :auto-sitemap nil
-         :makeindex nil
-         )
-
         ("wct-manual-pdf"
-         :base-directory "."
-         :publishing-directory "web"
+         :base-directory ".."
+         :publishing-directory "."
          :publishing-function (org-latex-publish-to-pdf)
          :base-extension "does-not-exist"
          :include ("manual.org")
@@ -54,8 +52,8 @@
          )
 
         ("wct-manual-md"
-         :base-directory "."
-         :publishing-directory "web"
+         :base-directory ".."
+         :publishing-directory "."
          :publishing-function (org-gfm-publish-to-gfm)
          :base-extension "does-not-exist"
          :include ("manual.org")
@@ -64,19 +62,6 @@
          :auto-sitemap nil
          :makeindex nil
          )
-
-        ("wct-manual-md"
-         :base-directory "."
-         :publishing-directory "web"
-         :publishing-function (org-pandoc-export-to-epub)
-         :base-extension "does-not-exist"
-         :include ("manual.org")
-         :recursive nil
-         :htmlized-source t
-         :auto-sitemap nil
-         :makeindex nil
-         )
-        
 
 
         ))
--- manuals/publish.sh
+++ manuals/publish.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+mydir=$(dirname $(readlink -f $BASH_SOURCE))
+cd $mydir
+
 
 /usr/bin/emacs --batch -Q -l publish.el \
                --eval '(org-publish-project "wct-manual")'
 /usr/bin/pandoc --toc-depth=2 -N -o web/manual.epub \
                 epub-title.txt web/manual.md
+cp *.org web/
+
+localweb="../../../wirecell.github.io"
+if [ -d $localweb ] ; then
+   echo "'deploying' to web"
+   cp -a web/* $localweb
+fi
+
