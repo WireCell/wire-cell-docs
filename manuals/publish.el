07371bc467be5cf4c8a289976d06edab6aaad37c
Brett Viren <brett.viren@gmail.com>
2017-05-29 16:24:42 -0400

    Clean up export customization to gain headline links.

--- manuals/publish.el
+++ manuals/publish.el
@@ -4,86 +4,70 @@
 (require 'ox-html)
 (message (format "org-mode version %s" org-version))
 
-;; ugly hack to inject some stuff into HTML headlines.  Maybe there is a better way to do this.
-(defun org-html-headline (headline contents info)
-  "Transcode a HEADLINE element from Org to HTML.
-CONTENTS holds the contents of the headline.  INFO is a plist
-holding contextual information."
-  (unless (org-element-property :footnote-section-p headline)
-    (let* ((numberedp (org-export-numbered-headline-p headline info))
-           (numbers (org-export-get-headline-number headline info))
-           (level (+ (org-export-get-relative-level headline info)
-                     (1- (plist-get info :html-toplevel-hlevel))))
-           (todo (and (plist-get info :with-todo-keywords)
-                      (let ((todo (org-element-property :todo-keyword headline)))
-                        (and todo (org-export-data todo info)))))
-           (todo-type (and todo (org-element-property :todo-type headline)))
-           (priority (and (plist-get info :with-priority)
-                          (org-element-property :priority headline)))
-           (text (org-export-data (org-element-property :title headline) info))
-           (tags (and (plist-get info :with-tags)
-                      (org-export-get-tags headline info)))
-           (full-text (funcall (plist-get info :html-format-headline-function)
-                               todo todo-type priority text tags info))
-           (contents (or contents ""))
-	   (ids (delq nil
-                      (list (org-element-property :CUSTOM_ID headline)
-                            (org-export-get-reference headline info)
-                            (org-element-property :ID headline))))
-           (preferred-id (car ids))
-           (extra-ids
-	    (mapconcat
-	     (lambda (id)
-	       (org-html--anchor
-		(if (org-uuidgen-p id) (concat "ID-" id) id)
-		nil nil info))
-	     (cdr ids) "")))
-      (if (org-export-low-level-p headline info)
-          ;; This is a deep sub-tree: export it as a list item.
-          (let* ((type (if numberedp 'ordered 'unordered))
-                 (itemized-body
-                  (org-html-format-list-item
-                   contents type nil info nil
-                   (concat (org-html--anchor preferred-id nil nil info)
-                           extra-ids
-                           full-text))))
-            (concat (and (org-export-first-sibling-p headline info)
-                         (org-html-begin-plain-list type))
-                    itemized-body
-                    (and (org-export-last-sibling-p headline info)
-                         (org-html-end-plain-list type))))
-        (let ((extra-class (org-element-property :HTML_CONTAINER_CLASS headline))
-              (first-content (car (org-element-contents headline))))
-          ;; Standard headline.  Export it as a section.
-          (format "<%s id=\"%s\" class=\"%s\">%s%s</%s>\n"
-                  (org-html--container headline info)
-                  (concat "outline-container-"
-			  (org-export-get-reference headline info))
-                  (concat (format "outline-%d" level)
-                          (and extra-class " ")
-                          extra-class)
-                  (format "\n<h%d id=\"%s\">%s%s%s</h%d>\n"
-                          level
-                          preferred-id
-                          extra-ids
-                          (format
-                                  "<a id=\"user-content-%s\" class=\"anchor\" href=\"#%s\" aria-hidden=\"true\"><svg aria-hidden=\"true\" class=\"octicon octicon-link\" height=\"16\" version=\"1.1\" viewBox=\"0 0 16 16\" width=\"16\"><path fill-rule=\"evenodd\" d=\"M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z\"></path></svg> </a>"
-                                  preferred-id preferred-id)
-                          (concat
-                           (and numberedp
-                                (format
-                                 "<span class=\"section-number-%d\">%s</span> "
-                                 level
-                                 (mapconcat #'number-to-string numbers ".")))
-                           full-text)
-                          level)
-                  ;; When there is no section, pretend there is an
-                  ;; empty one to get the correct <div
-                  ;; class="outline-...> which is needed by
-                  ;; `org-info.js'.
-                  (if (eq (org-element-type first-content) 'section) contents
-                    (concat (org-html-section first-content "" info) contents))
-                  (org-html--container headline info)))))))
+(defun wct-html-headline (headline contents info)
+  "Transcode headline from org to WCT HTML"
+  (let* ((text (and contents (org-html-headline headline contents info)))
+         (ids (delq nil
+                    (list (org-element-property :CUSTOM_ID headline)
+                          (org-export-get-reference headline info)
+                          (org-element-property :ID headline))))
+         (slug (car ids)))
+    (with-temp-buffer
+      (insert text)
+      (goto-char (point-min))
+      ;; this seems dodgy as fsck
+      (if (not (search-forward "<h" nil t))
+          text
+        (search-forward ">")
+        (insert
+         (format
+          "<a id=\"user-content-%s\" class=\"anchor\" href=\"#%s\" aria-hidden=\"true\"><svg aria-hidden=\"true\" class=\"octicon octicon-link\" height=\"16\" version=\"1.1\" viewBox=\"0 0 16 16\" width=\"16\"><path fill-rule=\"evenodd\" d=\"M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z\"></path></svg></a>"
+          slug slug))
+        (buffer-string)))))
+
+(defun wct-html-export-as-html
+    (&optional async subtreep visible-only body-only ext-plist)
+  "Export current buffer to an HTML buffer."
+  (interactive)
+  (org-export-to-buffer 'wct-html "*Org WCT HTML Export*"
+    async subtreep visible-only body-only ext-plist
+    (lambda () (set-auto-mode t))))
+
+(defun wct-html-export-to-html
+    (&optional async subtreep visible-only body-only ext-plist)
+    "Export current buffer to a HTML file."
+    (interactive)
+    (let* ((extension (concat "." (or (plist-get ext-plist :html-extension)
+                                      org-html-extension
+                                      "html")))
+           (file (org-export-output-file-name extension subtreep))
+           (org-export-coding-system org-html-coding-system))
+      (org-export-to-file 'wct-html file
+        async subtreep visible-only body-only ext-plist)))
+
+(defun wct-html-publish-to-html (plist filename pub-dir)
+    "Publish an org file to HTML."
+    (org-publish-org-to 'wct-html filename
+                        (concat "." (or (plist-get plist :html-extension)
+                                        org-html-extension
+                                        "html"))
+                        plist pub-dir))
+
+  
+
+(org-export-define-derived-backend 'wct-html 'html
+  :menu-entry
+  '(?w "Export to WCT HTML"
+       ((?H "As WCT HTML buffer" wct-html-export-as-html)
+        (?h "As WCT HTML file" wct-html-export-to-html)
+        (?o "As WCT HTML file and open"
+            (lambda (a s v b)
+              (if a (wct-html-export-to-html t s v b)
+                (org-open-file (wct-html-export-to-html nil s v b)))))))
+  :translate-alist '((headline . wct-html-headline)))
+       
+
+
 
 ;; https://github.com/fniessen/org-html-themes
 ;; $ emacs --batch -Q -l ../publish.el --eval '(org-publish-project "wct-manual")'
@@ -100,7 +84,7 @@ holding contextual information."
         ("wct-index-html"
          :base-directory ".."
          :publishing-directory "."
-         :publishing-function (org-html-publish-to-html)
+         :publishing-function (wct-html-publish-to-html)
          :base-extension "does-not-exist"
          :include ("index.org")
          :recursive nil
@@ -111,7 +95,7 @@ holding contextual information."
         ("wct-manual-html"
          :base-directory ".."
          :publishing-directory "."
-         :publishing-function (org-html-publish-to-html)
+         :publishing-function (wct-html-publish-to-html)
          :base-extension "does-not-exist"
          :include ("manual.org")
          :recursive nil
